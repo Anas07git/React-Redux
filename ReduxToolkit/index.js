fb9dda3bdfcd22935f92b91e370fbe08acd17e6c
Anas07git <anas.karatela@gmail.com>
2024-02-03 01:15:39 +0530

    toolkit

--- ReduxToolkit/index.js
+++ ReduxToolkit/index.js
@@ -1,4 +1,5 @@
 const store= require("./app/store")
+const { fetchUsers } = require("./features/user/userSlice")
 const cakeActions= require("./features/cake/cakeSlice").cakeActions
 const icecreamActions= require("./features/icecream/icecreamSlice").icecreamActions
 
@@ -7,13 +8,14 @@ console.log("Initial State", store.getState())
 
 const unsubscribe = store.subscribe(()=> console.log("Updated State", store.getState()))
 // const unsubscribe = store.subscribe(()=> {})
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(3))
-unsubscribe()
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(3))
+// unsubscribe()
+store.dispatch(fetchUsers())
